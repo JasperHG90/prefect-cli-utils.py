123eaf2c192c0324e14244d01311271d39792d06
Jasper Ginn <jasperginn@gmail.com>
2023-02-17 21:02:59 +0100

    feat(blocks): added GCP HMAC credentials

--- src/prefect_cli_utils/blocks.py
+++ src/prefect_cli_utils/blocks.py
@@ -5,3 +5,10 @@ class PropertyCache(Block):
     """Used by Funda pipelines to cache the last property seen during a run"""
 
     url: str
+
+
+class GcpHmacCredentials(Block):
+    """Used to store HMAC S3-compatible credentials"""
+
+    access_key_id: str
+    secret_access_key: str
